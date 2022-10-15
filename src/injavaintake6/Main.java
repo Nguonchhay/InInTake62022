c3c63fed70b6dce3204636959a2d0cee7549c727
Nguonchhay Touch <nguonchhay.touch@gmail.com>
2022-10-15 22:42:13 +0700

    [TASK] Week2 practice for task 5&6

--- src/injavaintake6/Main.java
+++ src/injavaintake6/Main.java
@@ -1,13 +1,23 @@
 package injavaintake6;
 
+import java.sql.Time;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Locale;
 import java.util.Scanner;
+import java.util.concurrent.TimeUnit;
 
 public class Main {
     public static void main(String[] args) {
 //        week2Task1();
 //        week2Task2();
 //        week2Task3();
-        week2Task4();
+//        week2Task4();
+//        week2Task5();
+        week2Task6();
+
     }
 
     public static void week2Task1() {
@@ -133,5 +143,67 @@ public class Main {
                 break;
         }
         System.out.println("Result = " + result);
+    }
+
+    public static void week2Task5() {
+        /**
+         * Solution:
+         * 1. Get user's name input and store in variable "name"
+         * 2. Get user's date of birth input and store in variable "birthDate"
+         * 3. Convert variable "birthDate" into Date object
+         * 4. Get current date and store in variable "currentDate"
+         * 5. Calculate age by currentDate - birthDate and store in variable "age"
+         * 6. Output user's name and age
+         */
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Name: ");
+        String name = scanner.nextLine();
+
+        System.out.print("Date of birth (mm/dd/yyyy): ");
+        String strBirthDate = scanner.nextLine();
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("MM/dd/yyyy", Locale.ENGLISH);
+        try {
+            Date birthDate = simpleDateFormat.parse(strBirthDate);
+            Calendar currentDate = Calendar.getInstance();
+            long timeDiff = currentDate.getTimeInMillis() - birthDate.getTime();
+            /**
+             * Time scales
+             * 1s = 1000ms
+             * 1m = 60s
+             * 1h = 60m
+             * 1d = 24h
+             * 1y = 365d
+             */
+            long days = TimeUnit.DAYS.convert(timeDiff, TimeUnit.MILLISECONDS);
+            long age = days / 365;
+
+            System.out.println("Welcome to " + name + "! you are " + age + " years old.");
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void week2Task6() {
+        /**
+         * Solution:
+         * 1. Get user input a sentence and store in "text" => Ex: HeLLO WOrld
+         * 2. Split "text" by space and store in array "arrText" => ["HeLLO", "WOrld"]
+         * 3. Convert each word into lowercase => ["hello", "world"]
+         * 4. Convert first letter of each word to uppercase => ["Hello", "World"]
+         * 4.1. Get first letter of each word and convert to uppercase => H
+         * 4.2. Replace the first letter to the word => h -> H => Hello
+         *     => hello = h + ello
+         *     => Hello = H + ello
+         */
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Sentence: ");
+        String text = scanner.nextLine();
+        String[] arrText = text.split(" ");
+        for (int i = 0; i < arrText.length; i++) {
+            String lowercaseText = arrText[i].toLowerCase();
+            String firstLetter = lowercaseText.substring(0, 1).toUpperCase();
+            String formattedWord = firstLetter + lowercaseText.substring(1);
+            System.out.print(formattedWord + " ");
+        }
     }
 }
